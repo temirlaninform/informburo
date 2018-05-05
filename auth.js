693c964a294dc49e83c7a6f39b36224cb286c917
Temirlan <t.sholpankulov@informburo.kz>
2018-05-05 14:11:53 +0600

    change conflict

--- auth.js
+++ auth.js
@@ -1,10 +1,10 @@
 function authenticate(login,password){
-	if (login === 'login' && password === 'password'){
+	if (login === 'login' || password === 'password'){
 
 	return 'You were logged in';
 	}
 	else{
-	return 'Login is incorrect';
+	return 'Login is incorrect!!!';
 	}
 }
 
